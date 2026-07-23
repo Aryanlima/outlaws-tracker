24e4fdfb6d9ae7a7a343721f153ed323b762208a
Aryanlima <97045311+Aryanlima@users.noreply.github.com>
2026-07-23 17:17:34 +0900

    supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://fjagazbjltrsrsxcgxxp.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZqYWdhemJqbHRyc3JzeGNneHhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0NTY3NDIsImV4cCI6MjA5NDAzMjc0Mn0.IgimknnMkDTIcnMZLsHGao3LjkzMm2yVwezSUmrDZI0'
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
+  console.error('Missing Supabase environment variables')
+}
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
